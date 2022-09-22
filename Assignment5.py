ed35e933d6ad80d77e07acd7404c83903c9adc84
Suraj Yadav <114130470+ErSuraj097@users.noreply.github.com>
2022-09-22 22:15:29 +0530

    Add files via upload

---
+++ Assignment5.py
@@ -0,0 +1,61 @@
+#.........................Assignment-5
+#............Answer-1
+n=2534
+print(n//10)
+
+#...........Answer-2
+n="2089"
+print(n[:-1])
+
+#................Answer-3
+n=10
+m=20
+s=n
+n=m
+m=s
+print(n)
+print(m)
+
+#.............Answer- 4
+x=int(input("Value of x :"))
+y=int(input("Exp y :"))
+z=x**y
+print("x power y =", z)
+
+#..............Answer- 5
+num=int(input("Enter the Three digit :"))
+while (num >= 10):
+    num = num // 10
+print('The first digit of number:', num)
+
+#...............Answer-6
+num1=int(input("Enter the Three digit :"))
+print("The midle digit of number :",(num1//10)%10)
+
+#..............Answer- 7
+num=int(input("Enter the Three digit :"))
+print('The last digit of number:', num%10)
+
+#..........Answer-8
+x= 1,2,3,4
+y= 4 in x
+print(y)
+
+#...........Answer-9
+x= 1,2,3,4
+y= 4 not in x
+print(y)
+
+#...............Answer 10
+x=2800
+y=4000
+w= x is y
+z= x is not y
+print(w)
+print(z)
+
+
+
+
+
+
