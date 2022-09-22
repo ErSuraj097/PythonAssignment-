faa19582cc70f1c88b3116c3cf690b195ce0bc3e
Suraj Yadav <114130470+ErSuraj097@users.noreply.github.com>
2022-09-22 18:14:05 +0530

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,51 @@
+
+#.............../Assignment- 3/
+
+#..........Answer 1
+
+num= 2800
+print(num, type(num))
+chng=str(num)
+print(chng, type(chng))
+
+x="2800"
+print(type(x))
+
+# ...........Answer 2
+y='m'
+print(ord(y))
+
+#...........Answer-3
+z=100
+print(chr(z))
+
+#............Answer-4
+a=10
+print(bin(a))
+
+#............Answer-5
+b=23
+print(oct(b))
+
+#..........Answer-6
+c=43
+print(hex(c))
+
+#...........Answer-7
+d=0b1100101
+print(d)
+
+#...........Answer-8
+e=0x2f
+print(e)
+
+#A..........nswer-9
+f=0o125
+print(f)
+
+#..........Answer-10
+g=0o25
+h=0x39
+print(bin(g+h))
+
+#completed assigment
