b17102ad546d139ae5d660433ddf6e4c22b3dbc2
Suraj Yadav <114130470+ErSuraj097@users.noreply.github.com>
2022-09-23 01:12:34 +0530

    Add files via upload

---
+++ Assignment9.py
@@ -0,0 +1,93 @@
+#.............Assignment 9
+
+#...........Answer -1
+from unicodedata import name
+
+
+n=int(input("Enter the Number :"))
+i=0
+while i<n:
+    print("MySirG")
+    i+=1
+print()
+
+#............Answer-2
+n=int(input("Enter the Number :"))
+j=1
+while j<=n:
+    print(j)
+    j+=1
+print()
+
+#..........Answer-3
+n=int(input("Enter the Number :"))
+j=n
+while j>=1:
+    print(j)
+    j-=1
+print()
+
+#..........Answer- 4
+
+n=int(input("Enter the Number :"))
+j=1
+while j<=n:
+    if j%2!=0:
+
+        print(j)
+    j+=1
+print()
+
+#............Answer-5
+n=int(input("Enter the Number :"))
+j=n
+while j>=1:
+    if j%2!=0:
+
+        print(j)
+    j-=1
+print()
+
+#.......Answer-6
+n=int(input("Enter the Number :"))
+j=1
+while j<=n:
+    if j%2==0:
+
+        print(j)
+    j+=1
+print()
+
+#......Answer-7'
+n=int(input("Enter the Number :"))
+j=n
+while j>=1:
+    if j%2==0:
+
+        print(j)
+    j-=1
+print()
+
+#........Answer-8
+n=int(input("Enter the Number :"))
+k=1
+while k<=n:
+    print(k*k)
+    k+=1
+print()
+
+#..........Answer-9
+n=int(input("Enter the Number :"))
+k=1
+while k<=n:
+    print(k*k*k)
+    k+=1
+print()
+
+#........Answer-10
+n=int(input("Enter the Number :"))
+k=1
+while k<=10:
+    print(k*n)
+    k+=1
+print()
