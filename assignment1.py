474bb75b23a2c89634966d2f4525ce9dc48c3a72
Suraj Yadav <114130470+ErSuraj097@users.noreply.github.com>
2022-09-22 11:39:54 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,44 @@
+
+#...........Assignment 1...........
+
+
+#..........Ans 1 
+#>>> python
+
+#..........Answer-2
+print("Hello Python") 
+
+#.......Answer3
+
+#>>>File.py
+
+#.........Answer 4
+print("Hello Python")
+
+#.........Answer 5
+print("Hello\nPython")
+#.........Answer 6
+print('"MySirG"')
+
+#..........Ans 7
+print('"Teacher''s Day"')
+
+#..........Ans 8
+a= "PythonAssignment"
+print(a)
+
+#..........Ans 9
+name="Krishna"
+age= 20
+qualification= "Graduate"
+yearofExp= 3.6
+
+
+#..........Ans 10
+name="Krishna"
+age= 20
+qualification= "Graduate"
+yearofExp= 3.6
+print(name,"\n",age,"\n",qualification,"\n",yearofExp)
+
+
