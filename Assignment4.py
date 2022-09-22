5c3c12b63efac87ec614fb3244f2926245fc8af7
Suraj Yadav <114130470+ErSuraj097@users.noreply.github.com>
2022-09-22 13:38:28 +0530

    Add files via upload

---
+++ Assignment4.py
@@ -0,0 +1,78 @@
+#...........Assignment 4
+
+#..................Answer 1...........
+
+name =input("Enter the name : ")
+print("My name is ",name)
+
+
+#...............Answer 2
+num = int(input("Enter the number "))
+print(num)
+
+
+#................Answer3
+a= int(input("Enter the 1st No. :"))
+b=int(input("Enter the 2nd No. :"))
+s=a+b
+print(a,"+",b,"=",s)
+
+
+#...........Answer4
+r=int (input("Radius="))
+z=(r*r)*3.4
+print("Area of circle :", z)
+
+
+#.......Answer 5
+n=int(input("Enter the number"))
+sq=n*n
+print("Squuare number: ",sq)
+
+
+#.............Assignment 6
+
+b=int(input("Enter tha base value : "))
+h=int(input("Enter tha base value : "))
+
+a=(h*b)/2
+print("Area of Tringle : ", a)
+
+
+#.............Assignment 7
+a= int(input("Enter the 1st No. :"))
+b=int(input("Enter the 2nd No. :"))
+c=int(input("Enter the 3rd No. :"))
+avg=(a+b+c)/3
+print("Average : ", avg)
+
+
+#...........Assignment 8
+p= int(input("Enter the principle bal :"))
+r=int(input("Enter the rate. :"))
+t=int(input("Enter the time :"))
+s=(p*r*t)/100
+print("SI : ", s)
+print("Total Amount after",n,"year:",k+l)
+
+
+#..........Assignment 9
+
+l= int(input("Enter the Length :"))
+w=int(input("Enter the Width. :"))
+h=int(input("Enter the Hieght :"))
+v=l*w*h
+print("Volume of cuboid : ", v)
+
+
+
+#.............assignment 10
+
+l=int(input("Enter tha Length : "))
+h=int(input("Enter tha Hieght : "))
+
+a=(l*h)
+print("Area of Rectangle : ", a)
+
+
+
