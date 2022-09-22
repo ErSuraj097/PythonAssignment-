3c333c8b01e2465b94ba92ccc333707b2d0ae21f
Suraj Yadav <114130470+ErSuraj097@users.noreply.github.com>
2022-09-23 01:06:08 +0530

    Add files via upload

---
+++ Assignment8.py
@@ -0,0 +1,84 @@
+#.............Assignment 8
+
+#...........Answer -1
+
+i=0
+while i<5:
+    print("MySirG")
+    i+=1
+print()
+
+#............Answer-2
+
+j=1
+while j<=10:
+    print(j)
+    j+=1
+print()
+
+#..........Answer-3
+j=10
+while j>=1:
+    print(j)
+    j-=1
+print()
+
+#..........Answer- 4
+j=1
+while j<=10:
+    if j%2!=0:
+
+        print(j)
+    j+=1
+print()
+
+#............Answer-5
+
+j=10
+while j>=1:
+    if j%2!=0:
+
+        print(j)
+    j-=1
+print()
+
+#.......Answer-6
+j=1
+while j<=10:
+    if j%2==0:
+
+        print(j)
+    j+=1
+print()
+
+#......Answer-7
+j=10
+while j>=1:
+    if j%2==0:
+
+        print(j)
+    j-=1
+print()
+
+#........Answer-8
+k=1
+while k<=10:
+    print(k*k)
+    k+=1
+print()
+
+#..........Answer-9
+k=1
+while k<=10:
+    print(k*k*k)
+    k+=1
+print()
+
+#........Answer-10
+k=1
+while k<=10:
+    print(k*5)
+    k+=1
+print()
+
+
