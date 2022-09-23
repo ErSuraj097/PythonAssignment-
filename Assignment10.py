39f505e4771675bfc7a33f369387d54ba126a0b8
Suraj Yadav <114130470+ErSuraj097@users.noreply.github.com>
2022-09-23 17:30:51 +0530

    Add files via upload

---
+++ Assignment10.py
@@ -0,0 +1,59 @@
+#.....................Assignment- 10...........
+
+#...........Answer-1 
+
+n=int(input("Enter the number :"))
+for i in range(n):
+    print("MySirG")
+
+#...........Answer-2
+n= int (input ("Enter the number "))
+for i in range(n+1):
+    if i>=1:
+        print(i)
+
+#...........Answer-3
+n= int (input ("Enter the number "))
+for i in range(n,0,-1):
+        print(i)
+
+#...........Answer-4
+n= int (input ("Enter the number "))
+for i in range(n+1):
+    if i%2!=0:
+        print(i)
+
+#...........Answer-5
+n= int (input ("Enter the number "))
+for i in range(n+1,0,-1):
+    if i%2!=0:
+        print(i) 
+
+#...........Answer-6
+n= int (input ("Enter the number "))
+for i in range(n):
+    if i%2==0:
+        print(i) 
+
+#...........Answer-7
+n= int (input ("Enter the number "))
+for i in range(n,0,-1):
+    if i%2==0:
+        print(i) 
+
+#...........Answer-8
+n= int (input ("Enter the number "))
+for i in range(n+1):
+    if i>=1:
+        print(i*i) 
+
+#...........Answer-9
+n= int (input ("Enter the number "))
+for i in range(n+1):
+    if i>=1:
+        print(i*i*i) 
+
+#...........Answer-10
+n =int(input("Enter the number :"))
+for i in range (1,10+1):
+    print(i*n)
