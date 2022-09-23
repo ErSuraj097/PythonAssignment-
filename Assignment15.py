2bc941ad6b58e4780917033ccac3516d77c39bf1
Suraj Yadav <114130470+ErSuraj097@users.noreply.github.com>
2022-09-23 23:16:31 +0530

    Add files via upload

---
+++ Assignment15.py
@@ -0,0 +1,54 @@
+
+#...................Assignment 15
+#..............Answer-1
+n='Hello Python...!'
+n1="Hi...."
+n2=123
+str(n2)
+print(n)
+print(n1)
+print(n2)
+
+#..............Answer-2
+ch="iNeuron"
+
+print(ch[:5])
+
+#...........Answer-3
+ch="Hello Learners"
+print(ch[2:5])
+
+#.........Answer-4
+a="Learning"
+b="Python"
+print("Concatenation of string : ",a+b)
+
+#..............Answer-5
+a="iNeuron"
+print(len(a))
+
+#...........Anmswer-6
+a="iNeuron"
+print(a[::-1])
+
+
+#...........Answer-7
+nam= "Python is an interpreter language "
+f="Python"
+if f in nam:
+    print("FOUND THIS SUBSTRING")
+else:
+    print("Not Found..!")
+
+#...........Answer-8
+g=("a1s24dc")
+print(g.isdigit())    
+
+#...........Answer-9
+f= (" sky979244")
+print(f.isalpha())
+
+#...........Answer-10
+i=2800
+print(str(i))
+print(type(str(i)))
